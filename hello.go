a109f49d791c5588ac3e81aad295e555c0d3c7d6
Thorsten Sideb0ard <thorsten.sideb0ard@gmail.com>
2014-04-27 18:20:44 -0700

    yup yup

--- hello.go
+++ hello.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"regexp"
+	"strings"
+)
+
+type Conch struct {
+  name string
+  }
 
 func main() {
-	fmt.Printf("Hullo wurld. I am AIGOR\n")
+        //c:= Conch{}
+	fmt.Printf("Hullo. I am AIGOR\n")
+	bio := bufio.NewReader(os.Stdin)
+	for {
+		line, err := bio.ReadBytes('\n')
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+		sline := strings.TrimRight(string(line), "\n")
+		// fmt.Printf(sline + "\n")
+		reply := getAnswer(sline)
+		//fmt.Println("Default is ", c)
+		fmt.Println(reply)
+	}
+}
+
+func getAnswer(q string) string {
+	r, _ := regexp.Compile(`^help$`)
+	rn, _ := regexp.Compile(`my name is (.*)`)
+	//fmt.Printf("In Answer section, i gots " + q)
+	if r.MatchString(q) == true {
+		return ("I AM AIGOR. NO HELP REQUIRED\n")
+	} else if rn.MatchString(q) {
+                nom := rn.FindStringSubmatch(q)
+                sentence := "i know yer name " + string(nom[1])
+                return (sentence)
+        } else {
+		return "Ima tha answer\n"
+	}
 }
