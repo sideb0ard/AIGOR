170a7763c03e4f8910e26301b1970ec565def905
Thorsten Sideb0ard <thorsten.sideb0ard@gmail.com>
2014-04-29 21:18:47 -0700

    hup

--- hello.go
+++ hello.go
@@ -3,19 +3,22 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/garyburd/redigo/redis"
 	"io"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
 type Conch struct {
-  name string
-  }
+	name string
+}
 
 func main() {
-        //c:= Conch{}
+	//c:= Conch{}
 	fmt.Printf("Hullo. I am AIGOR\n")
+
 	bio := bufio.NewReader(os.Stdin)
 	for {
 		line, err := bio.ReadBytes('\n')
@@ -34,16 +37,29 @@ func main() {
 }
 
 func getAnswer(q string) string {
+	tokens := strings.Split(q, " ")
+	c, err := redis.Dial("tcp", ":6379")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer c.Close()
+
+	for index, str := range tokens {
+		rkey := "aigor:reply:" + str
+		r, err := c.Do("SMEMBERS", rkey)
+		fmt.Println("NOM:" + strconv.Itoa(index) + " //VAL: " + str)
+		fmt.Println("REDIS REPLIES:" + r)
+	}
 	r, _ := regexp.Compile(`^help$`)
 	rn, _ := regexp.Compile(`my name is (.*)`)
 	//fmt.Printf("In Answer section, i gots " + q)
 	if r.MatchString(q) == true {
 		return ("I AM AIGOR. NO HELP REQUIRED\n")
 	} else if rn.MatchString(q) {
-                nom := rn.FindStringSubmatch(q)
-                sentence := "i know yer name " + string(nom[1])
-                return (sentence)
-        } else {
+		nom := rn.FindStringSubmatch(q)
+		sentence := "i know yer name " + string(nom[1])
+		return (sentence)
+	} else {
 		return "Ima tha answer\n"
 	}
 }
